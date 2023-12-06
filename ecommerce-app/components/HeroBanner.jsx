9b94342951af9a613373b893693bdeea228dd1fa
Pragatti <pragatti4667@gmail.com>
2023-12-06 23:57:55 +0530

    onadd-to-cart-logic

--- ecommerce-app/components/HeroBanner.jsx
+++ ecommerce-app/components/HeroBanner.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { urlFor } from '../lib/client'
 
 const HeroBanner = ({heroBanner}) => {
-    {console.log(heroBanner,"heroBanner")}
+   
   return (
     <div className='hero-banner-container'>
         <div>
