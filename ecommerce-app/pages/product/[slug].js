2c5b42155bceef5e3e6672437c777da3afaab113
Pragatti <pragatti4667@gmail.com>
2023-12-20 15:19:46 +0530

    done-feedback-chnages

--- ecommerce-app/pages/product/[slug].js
+++ ecommerce-app/pages/product/[slug].js
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
-import { Product, Products } from '../../components';
+import { Products } from '../../components';
 
 import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty,onAdd,setShowCart} = useStateContext();
-  
-  const handleBuyNow = () => {
-    onAdd(product, qty);
-    setShowCart(true);
-  }
+  const { decQty, incQty, qty,setShowCart,cartItems,showCartHandler} = useStateContext();
+  let isProductExist = cartItems?.find((item) => item._id === product?._id);
+
+  const handleBuyNow = (product, qty) => {
+    showCartHandler(true);
+    if (!isProductExist) {
+      addProductToCart(product, qty);
+    }
+  };
 
   return (
     <div>
@@ -52,16 +55,32 @@ const ProductDetails = ({ product, products }) => {
           <h4>Details: </h4>
           <p>{details}</p>
           <p className="price">${price}</p>
-          <div className="quantity">
-            <h3>Quantity:</h3>
-            <p className="quantity-desc">
-              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
-              <span className="num">{qty}</span>
-              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
-            </p>
-          </div>
+          {!isProductExist && (
+            <div className="quantity">
+              <h3>Quantity:</h3>
+              <p className="quantity-desc">
+                <span className="minus" onClick={decQty}>
+                  <AiOutlineMinus />
+                </span>
+                <span className="num">{qty}</span>
+                <span className="plus" onClick={incQty}>
+                  <AiOutlinePlus />
+                </span>
+              </p>
+            </div>
+          )}
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick= {()=>onAdd(product,qty)}>Add to Cart</button>
+          <button
+              type="button"
+              className="add-to-cart"
+              onClick={() =>
+                isProductExist
+                  ? setShowCart(true)
+                  : addProductToCart(product, qty)
+              }
+            >
+              {`${isProductExist ? "Go" : "Add"} to Cart`}
+            </button>
             <button type="button" className="buy-now" onClick= {handleBuyNow}>Buy Now</button>
           </div>
         </div>
